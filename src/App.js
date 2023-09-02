c76571b07004a36f647442a34f7432a37fcbfd18
AlanMandriaza <alan.mandriaza@gmail.com>
2023-09-02 05:25:34 +0000

    navbar agregado(por defecto aun)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import List from "./components/List";
 import CharacterCard from "./components/CharacterCard";
+import Navi from "./components/Navbar";
 
 const App = () => {
   return (
     <Router>
+      <Navi />
       <Routes>
         <Route path="/" element={<List />} />
         <Route path="/character/:id" element={<CharacterCard />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Navi = (props) => {
+  return (
+    <div>
+      <Navbar style={{backgroundColor: '#2f2f2f', color: '#b3b3b3', position: 'fixed', width: '100%', zIndex: 1000}} light expand="md">
+        <NavbarBrand href="/" style={{color: '#b3b3b3'}}>Navbar</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/" style={{color: '#b3b3b3'}}>Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap" style={{color: '#b3b3b3'}}>GitHub</NavLink>
+            </NavItem>
+          </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navi;
