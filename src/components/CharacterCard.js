37ad51fe1618cda0a49d26c5b65234ecbe0e31c7
Alan Mandriaza <alan.mandriaza@gmail.com>
2023-09-07 10:13:59 -0300

    estilos css agregados, proporcion de vista por personaje cambiada

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Button, Collapse } from 'reactstrap';
+import { CardImg, CardText, Dropdown, DropdownToggle, Collapse } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { fetchCharacterById } from './marvelAPI';
+import '../Styles/CharacterCard.css';
 
 const CharacterCard = () => {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [seriesOpen, setSeriesOpen] = useState(false);
+  const [comicsOpen, setComicsOpen] = useState(false);
+  const [eventsOpen, setEventsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+  const toggleSeries = () => setSeriesOpen(prev => !prev);
+  const toggleComics = () => setComicsOpen(prev => !prev);
+  const toggleEvents = () => setEventsOpen(prev => !prev);
 
   useEffect(() => {
     fetchCharacterById(id).then(setCharacter);
@@ -19,25 +24,54 @@ const CharacterCard = () => {
   }
 
   return (
-    <Card style={{ width: "18rem", marginLeft: "30px" }}>
-      <CardBody>
-        <CardTitle tag="h5">{character.name}</CardTitle>
-        <CardImg top width="100%" src={`${character.thumbnail.path.replace('http', 'https')}.${character.thumbnail.extension}`} alt={character.name} />
+    <div className="character-container">
+    <CardImg 
+      className="character-image"
+      src={`${character.thumbnail.path.replace('http', 'https')}.${character.thumbnail.extension}`} 
+      alt={character.name} 
+    />
+
+
+      <div style={{ flex: 1, marginLeft: '20px' }}>
+        <h5>{character.name}</h5>
         <CardText>
-          <p>{character.description}</p>
-          <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Series</Button>
-          <Collapse isOpen={isOpen}>
-            <Card>
-              <CardBody>
-                {character.series.items.map((serie, index) => (
-                  <li key={index}>{serie.name}</li>
-                ))}
-              </CardBody>
-            </Card>
-          </Collapse>
+          <p>{character.description || "No description available."}</p>
+
+          <Dropdown isOpen={seriesOpen} toggle={toggleSeries}>
+            <DropdownToggle caret>
+              Series
+            </DropdownToggle>
+            <Collapse isOpen={seriesOpen}>
+              {character.series.items.map((serie, index) => (
+                <li key={index}>{serie.name}</li>
+              ))}
+            </Collapse>
+          </Dropdown>
+
+          <Dropdown isOpen={comicsOpen} toggle={toggleComics} style={{ marginTop: '1rem' }}>
+            <DropdownToggle caret>
+              Comics
+            </DropdownToggle>
+            <Collapse isOpen={comicsOpen}>
+              {character.comics.items.map((comic, index) => (
+                <li key={index}>{comic.name}</li>
+              ))}
+            </Collapse>
+          </Dropdown>
+
+          <Dropdown isOpen={eventsOpen} toggle={toggleEvents} style={{ marginTop: '1rem' }}>
+            <DropdownToggle caret>
+              Events
+            </DropdownToggle>
+            <Collapse isOpen={eventsOpen}>
+              {character.events.items.map((event, index) => (
+                <li key={index}>{event.name}</li>
+              ))}
+            </Collapse>
+          </Dropdown>
         </CardText>
-      </CardBody>
-    </Card>
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-const Navi = (props) => {
+function MyNavbar() {
   return (
-    <div>
-      <Navbar style={{backgroundColor: '#2f2f2f', color: '#b3b3b3', position: 'fixed', width: '100%', zIndex: 1000}} light expand="md">
-        <NavbarBrand href="/" style={{color: '#b3b3b3'}}>Inicio</NavbarBrand>
+    <div style={{ marginBottom: '60px' }}>
+      <Navbar style={navbarStyle} light expand="md">
+        <NavbarBrand href="/" style={linkStyle}>Inicio</NavbarBrand>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink href="/components/" style={{color: '#b3b3b3'}}>null</NavLink>
+              <NavLink href="/components/" style={linkStyle}>Components</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap" style={{color: '#b3b3b3'}}>GitHub</NavLink>
+              <NavLink href="https://github.com/reactstrap/reactstrap" style={linkStyle}>GitHub</NavLink>
             </NavItem>
           </Nav>
       </Navbar>
@@ -19,4 +19,28 @@ const Navi = (props) => {
   );
 }
 
-export default Navi;
+// Estilos para el navbar y los links
+const navbarStyle = {
+  backgroundColor: '#2f2f2f',
+  color: '#b3b3b3',
+  position: 'fixed',
+  top: 0,
+  width: '100%',
+  zIndex: 1000,
+  borderBottom: '2px solid #b3b3b3',  
+  boxShadow: '0px 0px 10px 0px rgba(179,179,179,0.3)', 
+  backgroundColor: 'rgba(47, 47, 47, 0.9)'
+
+}
+
+const linkStyle = {
+  color: '#b3b3b3',
+  margin: '0 10px',  
+  transition: 'color 0.3s', 
+  '&:hover': {
+    color: '#ffffff',  
+    textDecoration: 'none'  
+  }
+}
+
+export default MyNavbar;
